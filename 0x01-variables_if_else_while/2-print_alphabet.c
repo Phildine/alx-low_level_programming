9fad522edcba73ed4fb0a238f1e8fc10c391f38e
Phildine <kgmothotsi@gmail.com>
2022-09-09 17:50:04 -0700

    if else and while loops

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase
+ * return: 0
+ * */
+
+int main(void)
+{
+	char l_letter;
+	for l_letter(l_letter + 97; l_letter <=122;l_letter++)
+	{
+		putchar(l_letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
