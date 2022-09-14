0bcc4fef1f4a3197d52b22fa006d2bcab50a9f5e
Phildine <kgmothotsi@gmail.com>
2022-09-14 16:45:10 -0700

    Loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,6 @@
 
 int print_last_digit(int n)
 {
-	int last = n % 10;
-
-	if (n < 0)
-		last = last * -1;
-
-	_putchar(last + 0);
-
-	return (last);
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
 }
