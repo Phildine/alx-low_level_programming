ea07f51ac6565f05187a98f238e445f852462caa
Phildine <kgmothotsi@gmail.com>
2022-09-13 19:00:22 -0700

    Hello loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,9 +13,9 @@ int main(void)
 {
 	_putchar('_');
 	_putchar('p');
-	_putchar('c');
+	_putchar('u');
 	_putchar('t');
-	_putchar('C');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
