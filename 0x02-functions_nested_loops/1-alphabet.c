7c413fc13b85e4d5012e828626e3dae76fbfa5d5
Phildine <kgmothotsi@gmail.com>
2022-09-13 19:18:05 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * prints alphabet in lower case folled by new line
+ *
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while(ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
