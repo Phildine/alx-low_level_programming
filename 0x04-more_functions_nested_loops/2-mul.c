a0d154bb32d236866394301bd1738f75c24bc177
Phildine <kgmothotsi@gmail.com>
2022-09-15 03:16:51 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - input
+ * escription: multiplies 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: the product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 Nested loops
+3-Prints numbers from 0-9
