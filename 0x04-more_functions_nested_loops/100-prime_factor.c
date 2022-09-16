edb70ec5861e8225dc3a56ce6630dd3aeafdb5e0
Phildine <kgmothotsi@gmail.com>
2022-09-15 19:23:09 -0700

    Lops' cle

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the biggest prime factor of a number.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int number;
+
+	number = 612852475143;
+
+	if (isPrime(number) == 1)
+	{
+		printf("%ld\n", number);
+	}
+	else
+	{
+		printf("%ld\n", biggestFactor(number));
+	}
+	return (0);
+}
+/**
