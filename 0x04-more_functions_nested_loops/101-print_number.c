e0cbeb00a13cca935d924e6b8a18d7c221ed66be
Phildine <kgmothotsi@gmail.com>
2022-09-16 17:26:28 -0700

    Loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,43 +1,67 @@
+#include <math.h>
 #include "main.h"
+
+/**
+ * power - exponents
+ * @base: base
+ * @exp: exponent
+ * Return: result (int)
+ */
+
+int power (int base, int exp)
+{
+	int i, num;
+
+	num = 1;
+	for (i = 0; i < exp; ++i)
+		num *= base;
+	return (num);
+}
+
 /**
  * print_number - print an int numbers.
- * @n: number tested
- * Return: Always 0.
+ * @n: numberto be printed
+ * Return: void
  */
+
 void print_number(int n)
 {
-	int i, j, digit, digits, power;
-	unsigned int temp, numchar, number;
+	int negative = 0;
+	int digit;
+	int divisor;
+	int begin = 0;
+	int place = 10;
 
-	digit = 0;
 	if (n < 0)
 	{
-		_putchar('-');
-		temp = -n;
+		negative = 1;
+		n = n * -1;
 	}
-	else
+	while (place >= 0)
 	{
-	number = temp;
-
-	while (number >= 10)
-	{
-		number = number / 10;
-		digit++;
+		/*divisor = power(10, place);*/
+		divisor = power(10, place);
+		digit = ((n / divisor) % 10);
+		if (digit == 0 && begin == 0)
+		{
+			place--;
+		}
+		else if (digit != 0 && begin == 0)
+		{
+			begin = 1;
+			if (negative == 1)
+				_putchar('-');
+			_putchar('0' + digit);
+			place--;
+		}
+		else
+		{
+			_putchar('0' + digit);
+			place--;
+		}
 	}
-	digits = digit + 1;
-	power = 1;
-	i = 1;
-
-	while (i < digits)
+	if (digit == 0 && divisor == 1)
 	{
-		power = power * 10;
-		i++;
-	}
-	j = power;
-	while (j >= 1)
-	{
-		numchar = (temp / j) % 10;
-		_putchar(numchar + '0');
-		j = j / 10;
+		_putchar(48);
 	}
 }
