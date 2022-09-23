cb935055da0ddca97190b90530cf8a51ade0702a
Phildine <kgmothotsi@gmail.com>
2022-09-22 19:13:08 -0700

    arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,30 @@
 #include "main.h"
-
 /**
- * cap_string - capitalize all word of a string
- * @s: string array
- * Return: string array
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
 
 char *cap_string(char *s)
 {
-	int i;
-	int prev;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		prev = i - 1;
-		if (s[i] >= 'a' && s[i] <= 'z')
+		for (i = 0; i < 13; i++)
 		{
-			if (i == 0)
-				s[i] = s[i] - 32;
-			else if (s[prev] >= 9 && s[prev] <= 10)
-				s[i] = s[i] - 32;
-			else if (s[prev] >= 32 && s[prev] <= 34)
-				s[i] = s[i] - 32;
-			else if (s[prev] >= 40 && s[prev] <= 41)
-				s[i] = s[i] - 32;
-			else if (s[prev] == 46)
-				s[i] = s[i] - 32;
-			else if (s[prev] == 59)
-				s[i] = s[i] - 32;
-			else if (s[prev] == 123 || s[prev] == 125)
-				s[i] = s[i] - 32;
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
 		}
+		count++;
 	}
-
 	return (s);
 }
